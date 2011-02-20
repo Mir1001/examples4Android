ed944035cad07d325b1158209deb011eb8dc588f
matejxxx <matej.crepinsek@gmail.com>
2011-02-20 23:16:52 +0100

    Test commit2

--- src/com/examples4Android/simple/Prvi.java
+++ src/com/examples4Android/simple/Prvi.java
@@ -1,5 +1,16 @@
 package com.examples4Android.simple;
 
 public class Prvi {
-  int i;
+	int i;
+	public Prvi() {
+		i=100;
+	}
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
 }
