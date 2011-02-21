5e6d68d1a5ed51d06ed41e913f3c8c5ccf06127c
matejxxx <matej.crepinsek@gmail.com>
2011-02-21 14:28:09 +0100

    Dodal stevec!

--- src/com/examples4Android/simple/HelloActivity.java
+++ src/com/examples4Android/simple/HelloActivity.java
@@ -7,12 +7,35 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class HelloActivity extends Activity {
-
+public class HelloActivity extends Activity implements OnClickListener {
+	Button inc, dec, show;
+	TextView tv_prikaz;
+	Stevec mojStevec;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         System.out.println("Moj test");
         setContentView(R.layout.main);
+        mojStevec = new Stevec();
+        inc = (Button) findViewById(R.id.btnInc);
+        dec = (Button) findViewById(R.id.btnDec);
+        show = (Button) findViewById(R.id.btnShow);
+        tv_prikaz = (TextView) findViewById(R.id.textViewPrikaz);
+        inc.setOnClickListener(this);
+        dec.setOnClickListener(this);
+        show.setOnClickListener(this);
+        
     }
+	@Override
+	public void onClick(View arg0) {
+		if (arg0.getId()==R.id.btnInc) {
+			mojStevec.inc();
+		} else
+			if (arg0.getId()==R.id.btnDec) {
+				mojStevec.dec();
+			} else
+				if (arg0.getId()==R.id.btnShow) {
+					tv_prikaz.setText(""+mojStevec.getStanje());
+				} 
+	}
 }
---
+++ src/com/examples4Android/simple/Stevec.java
@@ -0,0 +1,17 @@
+package com.examples4Android.simple;
+
+public class Stevec {
+  private int i;
+  public Stevec() {
+	  i=10;
+  }
+  public void inc() {
+	  i++;
+  }
+  public void dec() {
+	  i--;
+  }
+  public int getStanje() {
+	  return i;
+  }
+}
