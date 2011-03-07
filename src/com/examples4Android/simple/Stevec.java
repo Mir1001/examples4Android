52d2ed02e94a91c0559eabfb70f2892cc39eac2b
matejxxx <matej.crepinsek@gmail.com>
2011-03-07 10:33:56 +0100

    Test urejanja

--- src/com/examples4Android/simple/Stevec.java
+++ src/com/examples4Android/simple/Stevec.java
@@ -2,34 +2,35 @@ package com.examples4Android.simple;
 
 public class Stevec {
 	public static final String STEVEC_INC="PREF_INCREMENT_BUTTON";  //pref ime spremenljuvke
-  private int i;
-  
-  public Stevec() {
-	  i=10;
-  }
-  
-  public Stevec(int i) {
-	  this.i=i;
-  }
-  
-  public void inc() {
-	  i++;
-  }
-  
-  public void dec() {
-	  i--;
-  }
-  
-  public void setStanje(int i) {
-	  this.i=i;
-  }
-  
-  public void reset() {
-	  setStanje(0);
-  }
-  
-  public int getStanje() {
-	  return i;
-  }
-  
+	//
+	private int i;
+
+	public Stevec() {
+		i=10;
+	}
+
+	public Stevec(int i) {
+		this.i=i;
+	}
+
+	public void inc() {
+		i++;
+	}
+
+	public void dec() {
+		i--;
+	}
+
+	public void setStanje(int i) {
+		this.i=i;
+	}
+
+	public void reset() {
+		setStanje(0);
+	}
+
+	public int getStanje() {
+		return i;
+	}
+
 }
